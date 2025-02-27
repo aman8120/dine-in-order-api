7f100edd44eddc12bd05f822c8a1d23a0ac60e8c
aman8120 <amannagle2000@gmail.com>
2025-02-27 17:19:51 +0530

    Modifying - UserRepository

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.example.dio.repository;
 
 import com.example.dio.model.User;
-import com.example.dio.until.ResponseStructure;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User,Long>{
 
