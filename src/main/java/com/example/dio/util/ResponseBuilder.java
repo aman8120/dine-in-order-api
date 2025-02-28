afff696aca91ef2d2492aa2085968e204bad2c0e
aman8120 <amannagle2000@gmail.com>
2025-02-28 17:50:04 +0530

    Modfying ResponseBuilder CLass

--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -16,4 +16,18 @@ public class ResponseBuilder {
                 .body(structure);
     }
 
+    public  static  ResponseEntity<SimpleErrorResponse> error(HttpStatus status, String message){
+        SimpleErrorResponse error = SimpleErrorResponse.builder()
+                .type(status.name())
+                .message(message)
+                .status(status.value())
+                .build();
+
+        return  ResponseEntity.status(status).body(error);
+
+    }
+
+
+
+
 }
