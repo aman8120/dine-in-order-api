a6a177246a46073b648084f440cf0ec5a7b57d3c
aman8120 <amannagle2000@gmail.com>
2025-02-28 17:48:37 +0530

    Modifying Service interface

--- src/main/java/com/example/dio/service/UserService.java
+++ src/main/java/com/example/dio/service/UserService.java
@@ -1,8 +1,17 @@
 package com.example.dio.service;
 
+import com.example.dio.dto.request.RegistrationRequest;
+import com.example.dio.dto.request.UserRequest;
+import com.example.dio.dto.response.UserResponse;
 import com.example.dio.model.User;
 
 public interface UserService {
 
-   public User registerUser(User user);
+   public UserResponse registerUser(RegistrationRequest user);
+
+    public UserResponse findUserById(long id);
+
+   UserResponse updateUserById(long userId, UserRequest updatedUser);
+
+   void deleteUserById(long userId);
 }
