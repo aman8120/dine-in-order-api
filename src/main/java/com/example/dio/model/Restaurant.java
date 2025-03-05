f5056551475972ef876c4cdc70f4355ae435d894
aman8120 <amannagle2000@gmail.com>
2025-03-05 16:25:08 +0530

    Adding one entity in model pacakege Restaurant.java

---
+++ src/main/java/com/example/dio/model/Restaurant.java
@@ -0,0 +1,55 @@
+package com.example.dio.model;
+
+import com.example.dio.enums.DietType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "restaurant")
+@Getter
+@Setter
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private long restaurantId;
+
+    @Column(name = "restaurant_name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "contact_number")
+    private long contactNumber;
+
+    @Column(name = "contact_email")
+    private String contactEmail;
+
+    @Column(name = "opens_at")
+    private LocalTime opensAt;
+
+    @Column(name = "close_at")
+    private LocalTime closesAt;
+
+    @Column(name = "diet_type")
+    private DietType dietType;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "last_modified_at")
+    private LocalDate lastModifiedAt;
+
+    @ManyToMany(mappedBy = "restaurant", fetch = FetchType.EAGER)
+    private List<CuisineType> cuisineTypes;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Admin admin;
+}
