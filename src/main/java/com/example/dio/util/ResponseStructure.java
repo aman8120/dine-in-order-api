4bb4ebc5aaf185d1b704c528250086d870575fad
aman8120 <amannagle2000@gmail.com>
2025-03-05 14:29:40 +0530

    Adding Documentation

--- src/main/java/com/example/dio/util/ResponseStructure.java
+++ src/main/java/com/example/dio/util/ResponseStructure.java
@@ -13,6 +13,15 @@ public class ResponseStructure<T> {
     private String message;
     private T data;
 
+    /**
+     * Creates a new {@link ResponseStructure} instance with the given status, message, and data.
+     *
+     * @param status  The HTTP status of the response.
+     * @param message The message describing the response.
+     * @param data    The response data.
+     * @param <T>     The type of the response data.
+     * @return A new {@link ResponseStructure} instance with the provided values.
+     */
     public static <T> ResponseStructure<T> create(HttpStatus status, String message, T data) {
         ResponseStructure<T> response = new ResponseStructure<>();
         response.status = status.value();
