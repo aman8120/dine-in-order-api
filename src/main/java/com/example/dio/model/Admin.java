21a8b89e19d8612813bcd8aba17a4ce987532e6e
aman8120 <amannagle2000@gmail.com>
2025-03-05 15:54:51 +0530

    Adding Annotation @OneToMany in Admin entity

--- src/main/java/com/example/dio/model/Admin.java
+++ src/main/java/com/example/dio/model/Admin.java
@@ -1,9 +1,15 @@
 package com.example.dio.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name = "adims")
 public class Admin extends User {
+
+    @OneToMany
+    private List<Restaurant> restaurants;
 }
