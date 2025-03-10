ff08fcc7dd095b83998af254a0a880ce65e0e871
aman8120 <amannagle2000@gmail.com>
2025-03-10 15:04:00 +0530

    Adding FoodItemController endpoint in controller package

---
+++ src/main/java/com/example/dio/controller/FoodItemsController.java
@@ -0,0 +1,32 @@
+package com.example.dio.controller;
+
+import com.example.dio.dto.request.FoodItemRequest;
+import com.example.dio.dto.response.FoodItemResponse;
+import com.example.dio.model.Restaurant;
+import com.example.dio.service.FoodItemsService;
+import com.example.dio.util.ResponseBuilder;
+import com.example.dio.util.ResponseStructure;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class FoodItemsController {
+
+    private FoodItemsService foodItemsService;
+
+    @PostMapping("/restaurant/{restaurantId}/foodItems")
+    public ResponseEntity<ResponseStructure<FoodItemResponse>> registerFoods(@RequestBody @Valid FoodItemRequest foodItemsRequest, @PathVariable Restaurant restaurantId) {
+
+        FoodItemResponse foodItemsResponse = foodItemsService.registerFoods(foodItemsRequest, restaurantId);
+
+        return ResponseBuilder.success(HttpStatus.CREATED, "FoodItems Created", foodItemsResponse);
+
+    }
+}
