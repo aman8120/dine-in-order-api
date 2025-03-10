d2f57bdf3c71a0046ff9a8880165727caea33950
aman8120 <amannagle2000@gmail.com>
2025-03-10 23:50:59 +0530

    Modifying FoodItemRepository.java class

--- src/main/java/com/example/dio/repository/FoodItemRepository.java
+++ src/main/java/com/example/dio/repository/FoodItemRepository.java
@@ -2,6 +2,11 @@ package com.example.dio.repository;
 
 import com.example.dio.model.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
+
 }
