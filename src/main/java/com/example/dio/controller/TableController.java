9837e11b5290b01a2cd3391e13f053244b0fc32e
aman8120 <amannagle2000@gmail.com>
2025-03-10 14:52:00 +0530

    Adding TableController endPoint in controller package

---
+++ src/main/java/com/example/dio/controller/TableController.java
@@ -0,0 +1,32 @@
+package com.example.dio.controller;
+
+import com.example.dio.dto.request.TableRequest;
+import com.example.dio.dto.response.TableResponse;
+import com.example.dio.model.Restaurant;
+import com.example.dio.service.TableService;
+import com.example.dio.util.ResponseBuilder;
+import com.example.dio.util.ResponseStructure;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class TableController {
+
+    private TableService tableService;
+
+    @PostMapping("/restaurants/{restaurantId}/tables")
+    public ResponseEntity<ResponseStructure<TableResponse>> registerTable(@RequestBody @Valid TableRequest tableRequest, @PathVariable Restaurant restaurantId) {
+
+        TableResponse tableResponse = tableService.registerTable(tableRequest,restaurantId);
+
+        return ResponseBuilder.success(HttpStatus.CREATED, "Table Created", tableResponse);
+    }
+
+}
