9ec4070317dec27a6b9f83aa6fca58b97a4180d2
aman8120 <amannagle2000@gmail.com>
2025-03-10 23:47:55 +0530

    modifying registerFoodItem Method

--- src/main/java/com/example/dio/service/imp/FoodItemServiceImpl.java
+++ src/main/java/com/example/dio/service/imp/FoodItemServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.dio.dto.request.FoodItemRequest;
 import com.example.dio.dto.response.FoodItemResponse;
 import com.example.dio.exception.UserNotFoundByIdException;
 import com.example.dio.mapper.FoodItemMapper;
+
 import com.example.dio.model.CuisineType;
 import com.example.dio.model.FoodItem;
 import com.example.dio.model.Restaurant;
+import com.example.dio.repository.CategoryRepository;
 import com.example.dio.repository.CuisineTypeRepository;
 import com.example.dio.repository.FoodItemRepository;
 import com.example.dio.repository.RestaurantRepository;
@@ -15,17 +17,17 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @AllArgsConstructor
-@Transactional
+
 public class FoodItemServiceImpl implements FoodItemService {
     private FoodItemRepository foodItemRepository;
     private RestaurantRepository restaurantRepository;
     private CuisineTypeRepository cuisineTypeRepository;
     private FoodItemMapper foodItemsMapper;
+    private CategoryRepository categoryRepository;
 
+    @Transactional
     @Override
     public FoodItemResponse registerFoods(FoodItemRequest foodItemRequest, Restaurant restaurantId) {
 
@@ -38,6 +40,11 @@ public class FoodItemServiceImpl implements FoodItemService {
         foodItem.setRestaurant(existingRestaurant);
 
         foodItemRepository.save(foodItem);
+
+        if (!existingRestaurant.getCuisineTypes().contains(cuisineType.getCuisineName())) {
+            existingRestaurant.getCuisineTypes().add(foodItem.getCuisineType());
+            restaurantRepository.save(existingRestaurant);
+        }
 
         return foodItemsMapper.mapTOFoodItemsResponse(foodItem);
 
